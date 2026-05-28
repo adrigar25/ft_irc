aa9cfe012f9bd4d9a11092ba2804fb9c6530cef6
Adrián <adrigar250503@gmail.com>
2026-05-28 16:56:59 +0200

    commands: implementar PING handler y cabecera (responde PONG)

---
+++ src/server/commands/CmdPing.hpp
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*   CmdPing.hpp                                                               */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CMD_PING_HPP
+#define CMD_PING_HPP
+
+#include "commands/ICommand.hpp"
+#include "RequestContext.hpp"
+#include <string>
+
+class CmdPing : public ICommand {
+public:
+    CmdPing() {}
+    virtual ~CmdPing() {}
+    virtual void execute(RequestContext &ctx);
+};
+
+#endif
