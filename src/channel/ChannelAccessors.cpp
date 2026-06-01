f6bdf3475518259d30ba74a7bb406ab930a03762
Adrián <adrigar250503@gmail.com>
2026-06-01 16:51:57 +0200

    channel: dividir lógica en archivos Accessors/Checks/Roles/Users

---
+++ src/channel/ChannelAccessors.cpp
@@ -0,0 +1,140 @@
+#include "Channel.hpp"
+#include "User.hpp"
+#include "Exceptions.hpp"
+#include <map>
+
+void Channel::setIsInviteOnly(bool val)
+{
+    this->isInviteOnly = val;
+}
+
+void Channel::setUserLimit(int limit)
+{
+    this->userLimit = limit;
+}
+
+void Channel::setTopic(const std::string &t)
+{
+    this->topic = t;
+}
+
+void Channel::setKey(const std::string &k)
+{
+    this->key = k;
+}
+
+void Channel::setKeyRequired(bool val)
+{
+    this->keyRequired = val;
+}
+
+void Channel::setIsSecret(bool val)
+{
+    this->isSecret = val;
+}
+
+void Channel::setTopicProtected(bool val)
+{
+    this->topicProtected = val;
+}
+
+/**
+ * @brief Devuelve el número de usuarios en el canal.
+ */
+int Channel::getUserCount() const
+{
+    return this->userCount;
+}
+
+
+/**
+ * @brief Devuelve el límite de usuarios del canal, o -1 si no hay límite.
+ */
+int Channel::getUserLimit() const
+{
+    return this->userLimit;
+}
+
+
+/**
+ * @brief Indica si el canal es privado.
+ */
+bool Channel::getIsInviteOnly() const
+{
+    return this->isInviteOnly;
+}
+
+
+/**
+ * @brief Devuelve el nombre del canal.
+ */
+std::string Channel::getName() const
+{
+    return this->name;
+}
+
+
+/**
+ * @brief Devuelve el mapa de usuarios del canal.
+ */
+const std::map<int, User*>& Channel::getUsers() const
+{
+    return this->users;
+}
+
+
+/**
+ * @brief Devuelve la clave del canal.
+ */
+std::string Channel::getKey() const
+{
+    return this->key;
+}
+
+/**
+ * @brief Devuelve si el canal requiere clave.
+ */
+bool Channel::getKeyRequired() const
+{
+    return this->keyRequired;
+}
+
+bool Channel::getIsSecret() const
+{
+    return this->isSecret;
+}
+
+bool Channel::getTopicProtected() const
+{
+    return this->topicProtected;
+}
+
+std::string Channel::getTopic() const
+{
+    return this->topic;
+}
+
+const std::map<int, User*>& Channel::getOperators() const
+{
+    return this->operators;
+}
+
+const std::map<int, User*>& Channel::getVoiceUsers() const
+{
+    return this->voiceUsers;
+}
+
+const std::map<int, User*>& Channel::getInvitedUsers() const
+{
+    return this->invitedUsers;
+}
+
+const std::map<int, User*>& Channel::getBannedUsers() const
+{
+    return this->bannedUsers;
+}
+
+const std::map<int, User*>& Channel::getExceptedUsers() const
+{
+    return this->exceptedUsers;
+}
