64f22ee912dcda5db7d0aa5962c3bae378fd9997
Adrián <adrigar250503@gmail.com>
2026-06-22 18:32:59 +0200

    control de errores de argumentos del programa arreglado

--- mandatory/main.cpp
+++ mandatory/main.cpp
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: agarcia <agarcia@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/06/16 16:59:29 by agarcia           #+#    #+#             */
-/*   Updated: 2026/06/16 16:59:31 by agarcia          ###   ########.fr       */
+/*   Created: 2026/06/16 17:22:51 by adriescr          #+#    #+#             */
+/*   Updated: 2026/06/22 18:29:34 by agarcia          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,32 +21,31 @@
 #include "Server.hpp"
 #include <string>
 
-int	chekPort(const char* portStr, unsigned int &port) {
+int chekPort(const char *portStr, unsigned int &port)
+{
 	char *endptr = NULL;
 	long p = std::strtol(portStr, &endptr, 10);
-	if (*endptr != '\0' || p <= 0 || p > 65535) {
-		std::cerr << "Invalid port number. Please provide a port number between 1 and 65535." << std::endl;
+	if (*endptr != '\0' || p <= 0 || p > 65535)
+	{
+		std::cerr << "Invalid port number. Please provide a port number between 1 and 65535.\n(PORT set to default " << DEFAULT_PORT << ")" << std::endl;
 		return 1;
 	}
 	port = static_cast<unsigned int>(p);
 	return 0;
 }
 
-int	checkArgs(int argc, char* argv[], unsigned int &port, std::string &password) {
-	if (argc == 2) {
-		// Forma: ./ircserv <password>  -> usar puerto por defecto
-		port = DEFAULT_PORT;
-		password = argv[1];
-	} else if (argc == 3) {
-		// Forma: ./ircserv <port> <password>
-		if (chekPort(argv[1], port) != 0) {
-			return 1;
-		}
-		password = argv[2];
-	} else {
-		std::cerr << "Usage: " << argv[0] << " [port] <password>" << std::endl;
+int checkArgs(int argc, char *argv[], unsigned int &port, std::string &password)
+{
+
+	if (argc < 3 || argc > 3)
+	{
+		std::cerr << "Usage: " << argv[0] << " <port> <password>" << std::endl;
 		return 1;
 	}
+	if (chekPort(argv[1], port) != 0)
+		port = DEFAULT_PORT;
+	password = argv[2];
+
 	return 0;
 }
 
@@ -57,20 +56,26 @@ int	checkArgs(int argc, char* argv[], unsigned int &port, std::string &password)
  * y arranca el loop principal. Captura excepciones fatales y muestra un mensaje
  * antes de terminar.
  */
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[])
+{
 	unsigned int port;
 	std::string password;
 
 	if (checkArgs(argc, argv, port, password) != 0)
 		return 1;
 
-	try {
+	try
+	{
 		Server server(port, password);
 		server.startServer();
-	} catch (const std::exception &e) {
+	}
+	catch (const std::exception &e)
+	{
 		std::cerr << "Fatal: " << e.what() << std::endl;
 		return 1;
-	} catch (...) {
+	}
+	catch (...)
+	{
 		std::cerr << "Fatal: Unknown exception" << std::endl;
 		return 1;
 	}
